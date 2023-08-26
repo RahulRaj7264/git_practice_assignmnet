980a858203f2d738dcd92b522abeb34501dcbaf8
rahulrajsingh113@gmail.com <Rahul@7462>
2023-08-26 14:16:54 +0530

    Folder Upload

---
+++ palindrome.js
@@ -0,0 +1,13 @@
+let str ="Mihir";
+let str2 ="";
+for(let i=str.length-1;i>=0;i--){
+
+    str2+=str[i];
+
+}
+if(str2==str){
+    console.log("palindrom");
+}
+else{
+    console.log("not palindrom");
+}
